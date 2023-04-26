30e7c337ffb468e951dfb619e583f5062da3048f
patoag <pantinao.guairao@gmail.com>
2023-04-26 16:46:43 -0400

    creacion de modelo product, registro del modelo en mongodb

---
+++ models/Product.js
@@ -0,0 +1,9 @@
+const { model, Schema } = require('mongoose')
+
+const ProductSchema = new Schema({
+	title: { type: String, required: true },
+	description: String,
+	price: { type: Number, required: true },
+})
+
+export const Product = model('Product', ProductSchema)

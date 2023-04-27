4858f84151dfc2fc8c7ce20b81fd12fda47a11c4
patoag <pantinao.guairao@gmail.com>
2023-04-27 01:30:45 -0400

    se agrega opcion para cargar imagenes al producto, se logra hacer track del archivo, sin persistencia aun

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -7,6 +7,7 @@ export default function ProductForm({
 	title: existingTitle,
 	description: existingDescription,
 	price: existingPrice,
+	images,
 }) {
 	const [title, setTitle] = useState(existingTitle || '')
 	const [description, setDescription] = useState(existingDescription || '')
@@ -25,11 +26,32 @@ export default function ProductForm({
 			//create
 			await axios.post('/api/products', data)
 		}
-        setGoToProducts(true)
+		setGoToProducts(true)
 	}
 
 	if (goToProducts) {
 		router.push('/products')
+	}
+
+	async function uploadImages(ev) {
+		const files = ev.target?.files
+		if (files?.length > 0) {
+			const data = new FormData()
+
+			for (const file of files) {
+				data.append('file', file)
+			}
+
+			// const res = await axios.post('/api/upload', data, {
+            //     headers: {'Content-Type' : 'multipart/form-data'},
+            // })
+
+            const res = await fetch('/api/upload', {
+                method: 'POST',
+                body: data,
+            })
+			console.log(res)
+		}
 	}
 
 	return (
@@ -41,6 +63,33 @@ export default function ProductForm({
 				type="text"
 				placeholder="product name"
 			/>
+
+			<label>Photos</label>
+			<div>
+				<label className="w-24 h-24 text-center cursor-pointer flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+						/>
+					</svg>
+					<div>Upload</div>
+					<input
+						type="file"
+						onChange={uploadImages}
+						className="hidden"
+					/>
+				</label>
+				{!images?.length && <div>No photos in this product.</div>}
+			</div>
 
 			<label>Description</label>
 			<textarea
---
+++ pages/api/upload.js
@@ -0,0 +1,20 @@
+import multiparty from 'multiparty'
+
+export default async function handle(req, res) {
+	
+    const form = new multiparty.Form()
+
+    const {fields, files } = await new Promise((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if(err) reject(err)
+            resolve({fields, files})
+        })
+    })
+    console.log('length: ', files.file.length)
+    console.log(fields)
+    return res.json('ok!')
+}
+
+export const config = {
+	api: { bodyParser: false },
+}
